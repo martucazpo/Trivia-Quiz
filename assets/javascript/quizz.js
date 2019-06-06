d4ad076e7937c8458b82f09c946f17c57ba5a22f
MarcoTucsonpo <martucazpo@gmail.com>
2019-06-05 22:11:57 -0700

    still broken, never finished

--- assets/javascript/quizz.js
+++ assets/javascript/quizz.js
@@ -10,64 +10,59 @@ window.onload = function () {
     var numCorrect = 0;
 
     var output = [];
-    var answers = [];
+    var theAnswers=[];
 
     var myQuestions = [
         {
             question: "How much wood could a woodchuck chuck, if a woodchuck could chuck wood?",
-            answers: {
-                a: "about a cord and a half",
-                b: "three sticks if you're lucky",
-                c: "woodchucks are too lazy to chuck wood", 
-                d: "as much wood as a woodchuck would chuck",
-            },
-            correctAnswer: "d"
+            answers: [
+                "about a cord and a half",
+                "three sticks if you're lucky",
+                "woodchucks are too lazy to chuck wood",
+                "as much wood as a woodchuck would chuck",
+            ],
+            correctAnswer: 3
         },
         {
             question: "where does the rain in Spain fall?",
-            answers: {
-                a: "mainly down the drain",
-                b: "mainly on the plain",
-                c: "on zombies eating brains",
-                d: "on oragamiists folding cranes",
-            },
-            correctAnswer: "b"
+            answers: [
+                "mainly down the drain",
+                "mainly on the plain",
+                "on zombies eating brains",
+                "on oragamiists folding cranes",
+            ],
+            correctAnswer: 1
         },
         {
             question: "if a tree falls in the forest, and no-one hears it, did it fall?",
-            answers: {
-                a: "trees are too vain to fall when there is no-one to watch",
-                b: "yes, they are always drunk and fall over all the time",
-                c: "only if they fall on Schrodinger's cat",
-                d: "yes, they are too lazy to stand up",
-            },
-            correctAnswer: "c"
+            answers: [
+                "trees are too vain to fall when there is no-one to watch",
+                "yes, they are always drunk and fall over all the time",
+                "only if they fall on Schrodinger's cat",
+                "yes, they are too lazy to stand up",
+            ],
+            correctAnswer: 2
         }
     ];
 
     for (var i = 0; i < myQuestions.length; i++) {
 
 
-        for (letter in myQuestions[i].answers) {
+        theAnswers.push(
+            '<label>'
+            + '<input type="radio">'
+            + myQuestions[i].answers +
+            '</label>'
+        )
 
-            answers.push(
-                '<label>'
-                + '<input type="radio" name="question' + i + '" value="' + letter + '">'
-                + letter + ': '
-                + myQuestions[i].answers[letter]
-                + '</label>'
-            )
-        }
-    
+        output.push(
+            '<div class="question">' + myQuestions[i].question + '</div>'
+            + '<div class="answers">' + theAnswers.join('') + '</div>'
+        )
 
 
-    output.push(
-        '<div class="question">' + myQuestions[i].question + '</div>'
-        + '<div class="answers">' + answers.join('') + '</div>')
-    }    
-
-    quizContainer.innerHTML = output.join('');
-
+        quizContainer.innerHTML = output.join('');
+    }
 
     submitButton.onClick = function () {
 
@@ -86,6 +81,6 @@ window.onload = function () {
         resultsContainer.innerHTML = numCorrect + 'out of: ' + questions.length;
     }
 
+
+
 };
-
-
